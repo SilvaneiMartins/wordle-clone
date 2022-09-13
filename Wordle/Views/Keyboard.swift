35d479db8ea12f6d9fbf2ad6ea3f3579af9f6f7a
Silvanei Martins <silvaneimartins_rcc@hotmail.com>
2022-09-12 21:36:59 -0400

    Five letter words and verification

--- Wordle/Views/Keyboard.swift
+++ Wordle/Views/Keyboard.swift
@@ -19,11 +19,15 @@ struct Keyboard: View {
                 ForEach(topRowArray, id: \.self) { letter in
                     LetterButtonBiew(letter: letter)
                 }
+                .disabled(dm.disabledKeys)
+                .opacity(dm.disabledKeys ? 0.6 : 1)
             }
             HStack(spacing: 2) {
                 ForEach(secondRowArray, id: \.self) { letter in
                     LetterButtonBiew(letter: letter)
                 }
+                .disabled(dm.disabledKeys)
+                .opacity(dm.disabledKeys ? 0.6 : 1)
             }
             HStack(spacing: 2) {
                 Button {
@@ -35,9 +39,13 @@ struct Keyboard: View {
                 .frame(width: 60, height: 50)
                 .foregroundColor(.primary)
                 .background(Color.unused)
+                .disabled(dm.currentWord.count < 5 || !dm.inPlay)
+                .opacity((dm.currentWord.count < 5 || !dm.inPlay) ? 0.6 : 1)
                 ForEach(thirdRowArray, id: \.self) { letter in
                     LetterButtonBiew(letter: letter)
                 }
+                .disabled(dm.disabledKeys)
+                .opacity(dm.disabledKeys ? 0.6 : 1)
                 Button {
                     dm.removeLetterFromCurrentWord()
                 } label: {
@@ -47,6 +55,8 @@ struct Keyboard: View {
                 .frame(width: 40, height: 50)
                 .foregroundColor(.primary)
                 .background(Color.unused)
+                .disabled(!dm.inPlay || dm.currentWord.count == 0)
+                .opacity((!dm.inPlay || dm.currentWord.count == 0) ? 0.6 : 1)
             }
         }
     }
